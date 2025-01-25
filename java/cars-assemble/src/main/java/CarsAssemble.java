f363d509dce68c11d15d08feedf4a36f295e1a07
Christian Müller <smoki99@googlemail.com>
2025-01-25 06:10:27 +0100

    Simplified

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -2,17 +2,12 @@ public class CarsAssemble {
     final static double CARS_PRODUCED_EACH_HOUR=221;
     
     public double productionRatePerHour(int speed) {
-        return CARS_PRODUCED_EACH_HOUR * speed * getSuccessRate(speed);        
+        double successRate = 
+            speed <=4 ? 1 : speed <= 8 ? .9 : speed == 9 ? .8 : .77;
+        return CARS_PRODUCED_EACH_HOUR * speed * successRate;        
     }
 
     public int workingItemsPerMinute(int speed) {
         return (int) productionRatePerHour(speed)/60;
-    }
-
-    private double getSuccessRate(int speed) {
-        if (speed <=4) return 1;
-        if (speed<=8)  return 0.9;
-        if (speed==9)  return 0.8;
-        return 0.77;
     }
 }
